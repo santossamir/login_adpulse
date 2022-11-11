0ab22d68029d253fda9f59ce61f33c42c1d40a8d
Samir Santos <santossamir@hotmail.com>
2022-11-11 17:22:03 -0300

    Conectando ao banco de dados.

---
+++ app/conexao.php
@@ -0,0 +1,25 @@
+<?php
+    class Conexao {
+        
+        private $host = 'localhost';
+        private $dbname = 'db_adpulse';
+        private $user = 'root';
+        private $pass = 'root';
+
+        public function conectar(){
+            try{
+
+                $conexao = new PDO(
+                    "mysql:host=$this->host;dbname=$this->dbname",
+                    "$this->user",
+                    "$this->pass"
+                );
+
+                return $conexao;
+
+            } catch(PDOException $e){
+               echo '<p>' .$e->getMessage(). '</p>'; 
+            }
+        }
+    }
+?>
---
+++ public/js/index.js
@@ -0,0 +1,3 @@
+async function handleSubmitForm(e){
+    alert('Ok!');
+}
